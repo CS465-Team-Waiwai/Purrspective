8bc4dac97af82e86ba85652540ecbf45302a9daf
Aarnav8258 <148827784+Aarnav8258@users.noreply.github.com>
2025-12-04 15:51:39 -0600

    changed customization+back button

--- app/src/main/java/com/example/purrspective/CustomizationActivity.java
+++ app/src/main/java/com/example/purrspective/CustomizationActivity.java
@@ -1,9 +1,11 @@
 package com.example.purrspective;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.widget.Toolbar;
@@ -21,22 +23,28 @@ import androidx.core.content.ContextCompat;
 
 public class CustomizationActivity extends AppCompatActivity {
 
+    ImageView image1, image2;
+    Drawable greenOutline;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.customization);
 
-        RecyclerView rv = findViewById(R.id.rvHorizontal);
-        rv.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        image1 = findViewById(R.id.image_style1);
+        image2 = findViewById(R.id.image_style2);
 
-        int[] images = {
-                R.drawable.image1,
-                R.drawable.image2,
-                R.drawable.image3,
-                R.drawable.image4 // add as many as you want
-        };
+        greenOutline = getResources().getDrawable(R.drawable.green_outline);
 
-        rv.setAdapter(new ImageCarouselAdapter(images));
+        image1.setOnClickListener(v -> {
+            image1.setBackground(greenOutline);     // highlight
+            image2.setBackground(null);             // remove highlight
+        });
+
+        image2.setOnClickListener(v -> {
+            image2.setBackground(greenOutline);
+            image1.setBackground(null);
+        });
 
         /* TODO: Complete the functionality */
         Button saveButton = findViewById(R.id.save_button);
